3d79f0d161217c25051dfc79d38dd392eac0b28f
Tom Whitcomb <Tom_Whitcomb@intuit.com>
2014-07-01 15:11:36 -0700

    added more tests and fixed a delete bug

--- bst.go
+++ bst.go
@@ -1,4 +1,6 @@
 package bst
+import "fmt"
+import "strings"
 
 type Node struct{
   data interface{}
@@ -11,13 +13,14 @@ type comparer func(interface{},interface{}) int
 
 type BST struct {
   compare comparer
+  root *Node
 }
 
-func New(compare comparer) BST{
-  return BST{compare}
+func New(compare comparer) *BST{
+  return &BST{compare,nil}
 }
 
-func (bst BST) search(tree *Node,data interface{}) *Node {
+func (bst *BST) search(tree *Node,data interface{}) *Node {
   if tree == nil {
     return nil
   }
@@ -31,7 +34,18 @@ func (bst BST) search(tree *Node,data interface{}) *Node {
   }
   
 }
-func (bst BST) traverse(tree *Node) []*Node{
+
+func (bst *BST) print(tree *Node,level int){
+  if tree == nil {
+    return
+  }
+  bst.print(tree.right,level+1)
+  fmt.Printf("%s",strings.Repeat(".",level * 6))
+  fmt.Println(tree.data)
+  bst.print(tree.left,level+1)
+}
+
+func (bst *BST) traverse(tree *Node) []*Node{
   if tree == nil {
     return []*Node{}
   }
@@ -41,9 +55,13 @@ func (bst BST) traverse(tree *Node) []*Node{
   trees3 := bst.traverse(tree.right)
   return append(trees2,trees3...)
 }
-func (bst BST) insert(tree *Node,data interface{}) *Node {
+func (bst *BST) insert(tree *Node,data interface{}) *Node {
   if tree == nil {
-    return &Node{data,nil,nil,nil}
+    var node *Node = &Node{data,nil,nil,nil}
+    if bst.root == nil {
+      bst.root = node
+    }
+    return node
   }
   if bst.compare(data,tree.data) < 0 {
     tree.left = bst.insert(tree.left,data)
@@ -55,16 +73,50 @@ func (bst BST) insert(tree *Node,data interface{}) *Node {
   }
   return tree 
 }
+func (bst *BST) transplant(to *Node,from *Node){
+  if to.parent == nil {
+    bst.root = from
+  } else if to == to.parent.left {
+    to.parent.left = from
+  } else{
+    to.parent.right = from
+  }
+  if from != nil {
+    from.parent = to.parent
+  }
+}
 
-func (bst BST) delete(tree *Node,data interface{}) *Node{
+func (bst *BST) minimum(tree *Node) *Node{
+  for tree.left != nil {
+    tree = tree.left
+  }
+  return tree
+}
+
+func (bst *BST) maximum(tree *Node) *Node{
+  for tree.right != nil {
+    tree = tree.right
+  }
+  return tree
+}
+
+func (bst *BST) delete(tree *Node,data interface{}) *Node{
  
   target := bst.search(tree,data)
-  if target.left == nil && target.right == nil {
-    if target.parent.left == target {
-      target.parent.left = nil
-    } else{
-      target.parent.right = nil
+  if target.left == nil {
+    bst.transplant(target,target.right)
+  } else if target.right == nil {
+    bst.transplant(target,target.left)
+  } else {
+    y := bst.minimum(target.right)
+    if y.parent != target {
+      bst.transplant(y,y.right)
+      y.right = target.right
+      y.right.parent = y
     }
+    bst.transplant(target,y)
+    y.left = target.left
+    y.left.parent = y 
   }
   return tree
 }
--- bst_test.go
+++ bst_test.go
@@ -13,7 +13,31 @@ func compareInt(first interface{},second interface{}) int{
   }
 }
   
-func buildTree() (*Node,BST) {
+func compareChar(first interface{},second interface{}) int{
+  if first.(int32) < second.(int32){
+    return -1
+  } else if first.(int32) > second.(int32) {
+    return 1
+  } else {
+    return 0
+  }
+}
+func verifyTree(bst *BST,tree *Node) bool {
+  trees := bst.traverse(tree)
+  var min interface{} = nil
+  for _,value := range(trees){
+    if min == nil {
+      min = value.data
+    } else{
+      if bst.compare(min,value.data) != -1 {
+        return false
+      }
+      min = value.data
+    }
+  }
+  return true
+}  
+func buildTree() (*Node,*BST) {
   bst := New(compareInt)
   tree := bst.insert(nil,25)
   bst.insert(tree,15)
@@ -35,6 +59,9 @@ func buildTree() (*Node,BST) {
 
 func Test1(t *testing.T){
   tree,bst := buildTree()
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("Test1 - VerifyTree failed"))
+  }
   tree = bst.search(tree,66)
   b := tree.data == 66 && tree.parent.data == 70 && tree.parent.parent.data == 50 && tree.parent.parent.parent.data == 25
   if !b {
@@ -44,6 +71,9 @@ func Test1(t *testing.T){
 }
 func Test2(t *testing.T){
   tree,bst := buildTree()
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("Test2 - VerifyTree failed"))
+  }
   tree = bst.search(tree,100)
 
   if tree != nil {
@@ -53,6 +83,9 @@ func Test2(t *testing.T){
 }
 func Test3(t *testing.T){
   tree,bst := buildTree()
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("Test3 - VerifyTree failed"))
+  }
   trees := bst.traverse(tree)
   var s string = ""
   for _,tree := range trees{
@@ -65,6 +98,9 @@ func Test3(t *testing.T){
 
 func Test4(t *testing.T){
   tree,bst := buildTree()
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("Test4 - VerifyTree failed"))
+  }
   bst.delete(tree,90)
   trees := bst.traverse(tree)
   var s string = ""
@@ -74,6 +110,104 @@ func Test4(t *testing.T){
   if s != "4 10 12 15 18 22 24 25 31 35 44 50 66 70 " {
     t.Error(fmt.Sprintf("Test4 - deleted failed. got %s",s))
   }
-
-  
 }
+func TestDelete1(t *testing.T){
+  bst := New(compareChar)
+  tree := bst.insert(nil,'a')
+  bst.insert(tree,'b')
+  bst.insert(tree,'c')
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete1 - VerifyTree 1 failed"))
+  }
+  bst.delete(tree,'b')
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete1 - VerifyTree 1 failed"))
+  }
+}
+func TestDelete2(t *testing.T){
+  bst := New(compareChar)
+  tree := bst.insert(nil,'c')
+  bst.insert(tree,'b')
+  bst.insert(tree,'a')
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete2 - VerifyTree 1 failed"))
+  }
+  bst.delete(tree,'b')
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete2 - VerifyTree 2 failed"))
+  }
+}
+func TestDelete3(t *testing.T){
+  bst := New(compareInt)
+  tree := bst.insert(nil,5)
+  bst.insert(tree,8)
+  bst.insert(tree,7)
+  bst.insert(tree,9)
+  bst.insert(tree,10)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete3 - VerifyTree 1 failed"))
+  }
+  bst.delete(tree,8)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete3 - VerifyTree 2 failed"))
+  }
+}
+
+func TestDelete4(t *testing.T){
+  tree,bst := buildTree()
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete4 - VerifyTree 1 failed"))
+  }
+  bst.delete(tree,70)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete4 - VerifyTree 2 failed"))
+  }
+}
+func TestDelete5(t *testing.T){
+  tree,bst := buildTree()
+  bst.print(tree,0)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete5 - VerifyTree 1 failed"))
+  }
+  bst.delete(tree,15)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete5 - VerifyTree 2 failed"))
+  }
+  fmt.Println("========================")
+  bst.print(tree,0)
+}
+
+func TestDelete6(t *testing.T){
+  tree,bst := buildTree()
+  bst.print(tree,0)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete6 - VerifyTree 1 failed"))
+  }
+  fmt.Println("========================")
+  bst.delete(tree,15)
+  bst.print(tree,0)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete5 - VerifyTree 2 failed"))
+  }
+  fmt.Println("========================")
+  bst.delete(tree,18)
+  bst.print(tree,0)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete5 - VerifyTree 3 failed"))
+  }
+  fmt.Println("========================")
+  bst.delete(tree,10)
+  bst.print(tree,0)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete5 - VerifyTree 4 failed"))
+  }
+  fmt.Println("========================")
+  bst.delete(tree,25)
+  bst.print(tree,0)
+  if ! verifyTree(bst,tree) {
+     t.Error(fmt.Sprintf("TestDelete5 - VerifyTree 5 failed"))
+  }
+}
+
+
+
